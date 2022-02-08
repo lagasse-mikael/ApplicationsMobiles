f248b9604463f21acf6aa5bb6aeddcf1c1667d4e
mlagasse-cstj <2068410@cstj.qc.ca>
2022-02-08 10:11:13 -0500

    S02 Contraint Layout

---
+++ Semaine1/DeuxiemeProjet/app/src/main/java/ca/mikk/s02constraintlayout/PilotActivity.kt
@@ -0,0 +1,11 @@
+package ca.mikk.s02constraintlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PilotActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pilot)
+    }
+}
