91b6a560c3a8295ad0053bfae6dc56d68f1a2bac
Mik la brute <2068410@cstj.qc.ca>
2022-02-10 17:02:16 -0500

    Fin de la classe Pilot , y faut que sa spin asteur.

---
+++ Semaine1/DeuxiemeProjet/app/src/main/java/ca/mikk/s02constraintlayout/domain/models/Pilot.kt
@@ -0,0 +1,32 @@
+package ca.mikk.s02constraintlayout.domain.models
+
+import kotlin.random.Random
+
+data class Pilot(var name:String,var life: Int,var cube:Int = 0) {
+
+    private var _experince: Int = 0
+    var shield: Int = 5
+    var energy: Int = 7
+
+    val level:Int get() = _experince / 10
+
+    fun fly(revolutions:Int, isTraining:Boolean){
+        if(!isTraining){
+            _experince += revolutions * Random.nextInt(1,6)
+            life -= Random.nextInt(0,2)
+            shield -= Random.nextInt(0,6)
+            energy--
+            cube += Random.nextInt(0,revolutions + 1)
+        }
+    }
+
+    fun canFly():Boolean{
+        return life > 0 && energy > 0
+    }
+
+    // TO DO , constantes
+    fun recharge(){
+        energy = 7
+
+    }
+}
