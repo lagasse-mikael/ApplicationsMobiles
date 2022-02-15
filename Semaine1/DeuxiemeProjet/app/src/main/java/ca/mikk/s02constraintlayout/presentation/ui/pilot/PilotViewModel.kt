518d7ee17c1b1bc03cc698620764fcb1523433c1
mlagasse-cstj <2068410@cstj.qc.ca>
2022-02-15 10:40:25 -0500

    Debut des View Models

---
+++ Semaine1/DeuxiemeProjet/app/src/main/java/ca/mikk/s02constraintlayout/presentation/ui/pilot/PilotViewModel.kt
@@ -0,0 +1,18 @@
+package ca.mikk.s02constraintlayout.presentation.ui.pilot
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import ca.mikk.s02constraintlayout.domain.models.Pilot
+
+class PilotViewModel : ViewModel(){
+
+    private val _pilot = MutableLiveData<Pilot>()
+
+    val pilot : LiveData<Pilot> get() = _pilot
+
+    init {
+        _pilot.value = Pilot("Bee Zoon",10,10)
+    }
+
+}
